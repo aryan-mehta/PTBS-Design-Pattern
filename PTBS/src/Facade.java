6c17920ed4310c08bfcec9440a371d4e5cfe1717
aryan-mehta <aryancountry@gmail.com>
2022-10-19 16:54:47 -0700

    login functionality added

--- PTBS/src/Facade.java
+++ PTBS/src/Facade.java
@@ -2,53 +2,74 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+//Facade Design Pattern
 public class Facade {
 
-	private int UserType; //Buyer:0, Seller:1
+	private int UserType; // Buyer:0, Seller:1
 
-	private Product theSelectedProduct; //object that holds currently selected product
+	private Product theSelectedProduct; // object that holds currently selected product
 
-	private int nProductCategory; //Selected product category - Meat:0, Produce:1
+	private int nProductCategory; // Selected product category - Meat:0, Produce:1
 
 	private ClassProductList theProductList; // List of products of entire system
 
-	private Person thePerson; //the current logged in person
+	private Person thePerson; // the current logged in person
 
-	//show login gui and return login result
+	// show login gui and return login result
 	public boolean login() throws FileNotFoundException {
-		File file = new File("E:\\ARYAN\\Desktop\\ASU\\Sem1 Fall22\\SER515 - Fondations of Software Engineering\\Assignments\\Design Pattern Assignment\\BuyerInfo.txt");
-		Scanner sc = new Scanner(file);
-		Scanner scan = new Scanner(System.in);
-		String username = scan.nextLine();
-		String password = scan.nextLine();
-
-		while (sc.hasNextLine()) {
-			String line = sc.nextLine();
-			String[] arr = line.split(":");
-			String db_username = arr[0];
-			String db_password = arr[1];
-			if (username.equals(db_username) && password.equals(db_password)) {
-				return true;
+		System.out.println("Type of User?\nEnter 0 for Buyer\n 1 for Seller: ");
+		Scanner s = new Scanner(System.in);
+		UserType = s.nextInt();
+		int flag = 0;
+		Scanner sc;
+		if (UserType == 0) {
+			File file = new File("BuyerInfo.txt");
+			sc = new Scanner(file);
+			flag = 1;
+		} else if (UserType == 1) {
+			File file = new File("SellerInfo.txt");
+			sc = new Scanner(file);
+			flag = 1;
+		} else {
+			System.out.println("Invalid Input!\n Please enter correct UserType...");
+			login();
+		}
+		if (flag == 1) {
+			Scanner scan = new Scanner(System.in);
+			String username = scan.nextLine();
+			String password = scan.nextLine();
+			scan.close();
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				String[] arr = line.split(":");
+				String db_username = arr[0];
+				String db_password = arr[1];
+				if (username.equals(db_username) && password.equals(db_password)) {
+					return true;
+				}
 			}
 		}
 		sc.close();
-		scan.close();
+		s.close();
 		return false;
+
 	}
 
-	// call this function when clicking the addButton in ProductMenu. This function will add a new trade and fill the
-	//required information. SellerTradingMenu and BuyerTradingMenu will be called according to the type of user logged in.
+	// call this function when clicking the addButton in ProductMenu. This function
+	// will add a new trade and fill the
+	// required information. SellerTradingMenu and BuyerTradingMenu will be called
+	// according to the type of user logged in.
 	public void addTrading() {
 
 	}
 
-	//call this function when clicking the viewButton in ProductMenu. 
-	//Pass Trading pointer and Person pointer to this function.
-	//The function views the SellerTradingMenu or BuyerTradingMenu based on type of user logged in
+	// call this function when clicking the viewButton in ProductMenu.
+	// Pass Trading pointer and Person pointer to this function.
+	// The function views the SellerTradingMenu or BuyerTradingMenu based on type of
+	// user logged in
 	public void viewTrading() {
 
 	}
-
 
 	public void decideBidding() {
 
@@ -58,35 +79,38 @@ public class Facade {
 
 	}
 
-	//show the remind box to remind buyer of the upcoming overdue trading window.
+	// show the remind box to remind buyer of the upcoming overdue trading window.
 	public void remind() {
 
 	}
 
-	//create a user object aaccording to the userinfoitem, the object can
-	//be a buyer or a seller.
+	// create a user object aaccording to the userinfoitem, the object can
+	// be a buyer or a seller.
 	public void createUser(UserInfoItem userinfoitem) {
 
 	}
 
-	//create product list of the entire system.
+	// create product list of the entire system.
 	public void createProductList() {
+		}
 
 	}
 
-	//call this function after creating the user. Create productList by reading
-	//the UserProduct.txt file. Match the product name with theProductList. 
-	//Attach the matched product object to the new create user: Facase.thePerson.ProductList
+	// call this function after creating the user. Create productList by reading
+	// the UserProduct.txt file. Match the product name with theProductList.
+	// Attach the matched product object to the new create user:
+	// Facase.thePerson.ProductList
 	public void AttachProductToUser() {
 
 	}
 
-	//show the product list in the dialog and return the selected product.
+	// show the product list in the dialog and return the selected product.
 	public Product SelectProduct() {
 		return null;
 	}
 
-	// calls thePerson.CreateProductMenu() and call different menu creator to show the menu acc to type of user
+	// calls thePerson.CreateProductMenu() and call different menu creator to show
+	// the menu acc to type of user
 	public void productOperation() {
 
 	}
--- PTBS/src/Person.java
+++ PTBS/src/Person.java
@@ -1,5 +1,6 @@
+//Bridge Pattern - abstract class on one side of the bridge
 abstract class Person {
-	// use this variable to point to a concrete produtMenu object.
+	// use this variable to point to a concrete productMenu object.
 	private ProductMenu theProductMenu;
 
 	/**
@@ -10,6 +11,7 @@ abstract class Person {
 	/**
 	 *  
 	 */
+	//Call this function to show the 'Add' Button
 	public void showAddButton() {
 		System.out.println("Show Add Button");
 	}
@@ -17,17 +19,21 @@ abstract class Person {
 	/**
 	 *  
 	 */
+	//Call this function to show the 'View' Button
 	public void showViewButton() {
 		System.out.println("Show View Button");
 	}
 
+	//Call this function to show the 'Radio' Button
 	public void showRadioButton() {
 		System.out.println("Show Radio Button");
 	}
 
+	//Call this function to show the 'Labels'
 	public void showLabels() {
 		System.out.println("Show Labels");
 	}
 
+	//The Abstract Factory Method - According to the product type create product menu
 	public abstract ProductMenu CreateProductMenu();
 }
